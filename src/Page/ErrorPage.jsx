b50f202bceec6054222deea747df55e887c45231
introtoxicXXl <minhajulabedin648@gmail.com>
2023-11-21 18:58:10 +0600

    error page complete

--- src/Page/ErrorPage.jsx
+++ src/Page/ErrorPage.jsx
@@ -1,13 +1,28 @@
-import { useRouteError } from "react-router-dom";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
     const error = useRouteError()
-    console.error(error.message)
-    return (
-        <div>
-            <h1>{error.message}</h1>
-            <h1>{error.statusText}</h1>
+    if (isRouteErrorResponse(error)) {
+        if (error.status === 404) {
+            return <div className="h-screen flex justify-center items-center text-5xl text-red-700 font-bold">{error.status} This page does not exist!</div>;
+        }
 
+        if (error.status === 401) {
+            return <div className="h-screen flex justify-center items-center text-5xl text-red-700 font-bold">{error.status} You are not authorized to see this</div>;
+        }
+
+        if (error.status === 503) {
+            return <div className="h-screen flex justify-center items-center text-5xl text-red-700 font-bold">{error.status} Looks like our API is down</div>;
+        }
+
+        if (error.status === 418) {
+            return <div className="h-screen flex justify-center items-center text-5xl text-red-700 font-bold">{error.status} 🫖</div>;
+        }
+    }
+
+    return (
+        <div className="h-screen flex justify-center items-center text-5xl text-red-700 font-bold">
+            {error.status} {error.message}
         </div>
     );
 };
